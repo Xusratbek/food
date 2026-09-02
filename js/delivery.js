7f92ff7f167098449650dbfed266cbd4cf09d82f
Xusratbek <matsapayevxusrat@gmail.com>
2026-09-02 12:12:10 +0500

    update delevery.html food.html

--- js/delivery.js
+++ js/delivery.js
@@ -1,3 +1,6 @@
+
+
+
 import { menuCategories, menuItems } from "./menu-data.js";
 
 const deleveryLinks = document.getElementById("categoriesNav");
@@ -20,6 +23,82 @@ function renderProducts(items) {
     </a>
   `).join("");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    // ===== Booking Modal =====
+        const openBtn = document.getElementById('openModal');
+    const closeBtn = document.getElementById('closeModal');
+    const modal = document.getElementById('bookingModal');
+
+    if (closeBtn && modal) {
+      closeBtn.addEventListener('click', () => {
+        modal.classList.remove('active');
+      });
+
+      modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+          modal.classList.remove('active');
+        }
+      });
+    }
+
+    if (openBtn && modal) {
+      openBtn.addEventListener('click', () => {
+        modal.classList.add('active');
+      });
+    }
+  
+    
+
+    const openModalMobile = document.getElementById('openModalMobile');
+    if (openModalMobile && modal) {
+      openModalMobile.addEventListener('click', () => {
+        modal.classList.add('active');
+      });
+    }
+  
+    // ===== Mobile Menu =====
+    
+    const openMobileBtn = document.getElementById('openMobileMenu');
+    const openMobileBtnNav = document.getElementById('openMobileMenuNav'); // yangi qo'shildi
+    const closeMobileBtn = document.querySelector('#closeMobileMenu');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const openModalFromMenu = document.getElementById('openModalFromMenu');
+    
+    if (closeMobileBtn && mobileMenu) {
+      
+      const openMobileMenuHandler = (e) => {
+        e.preventDefault();
+        mobileMenu.classList.add('active');
+      };
+    
+      if (openMobileBtn) {
+        openMobileBtn.addEventListener('click', openMobileMenuHandler);
+      }
+      if (openMobileBtnNav) {
+        openMobileBtnNav.addEventListener('click', openMobileMenuHandler);
+      }
+    
+      closeMobileBtn.addEventListener('click', () => {
+        mobileMenu.classList.remove('active');
+      });
+    
+      if (openModalFromMenu) {
+        openModalFromMenu.addEventListener('click', () => {
+          mobileMenu.classList.remove('active');
+          modal.classList.add('active');
+        });
+      }
+    }
+  
+    
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        if (modal) modal.classList.remove('active');
+        if (mobileMenu) mobileMenu.classList.remove('active');
+      }
+    });
+  });
 
 
 function categoryFilter(categoryName) {
